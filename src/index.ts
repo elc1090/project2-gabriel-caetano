d1ffd99147a29854d2b151120d0dca17f4614c02
Gabriel Caetano <gvcaetano@inf.ufsm.br>
2023-04-18 11:47:27 -0300

    feat: start of modeling project

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,17 @@ import express from 'express'
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/tournaments', (req, res) => {
+  res.send('get list of tournaments')
+})
+
+app.get('/load-tournament-data/:id', (req, res) => {
+  req.params.id
+  res.send('load data from the tournament with id = ' + req.params.id)
+})
+
+app.get('/check-tournament-loaded/:id', (req, res) => {
+  res.send('return data from the tournament with id = ' + req.params.id)
 })
 
 app.listen(port, () => {
